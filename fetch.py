cc17d563a1271ae7b9881aedd8b39055f7be828a
lunnlew <lunnlew@qq.com>
2019-04-11 14:06:42 +0800

    update

--- fetch.py
+++ fetch.py
@@ -6,4 +6,18 @@ import worker
 import sqlite_data
 
 sqlite_data.init_data()
+
+# 省级
+# worker.fetch_provinces('2018')
+
+# 省级 地市级
+# worker.fetch_cities('2018')
+
+# 省级 地市级 区县级
+# worker.fetch_counties('2018')
+
+# 省级 地市级 区县级 乡镇街道级
+# worker.fetch_towns('2018')
+
+# 省级 地市级 区县级 乡镇街道级 村委居委级
 worker.fetch_villages('2018')
